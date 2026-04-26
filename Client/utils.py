60eff1e924c33df67367be17e020abbb0b6005a1
Menkib <menkib64@proton.me>
2026-04-27 00:46:45 +0300

    Enable pro mode to allow all options

--- Client/utils.py
+++ Client/utils.py
@@ -151,7 +151,7 @@ def read_git_credentials(engine):
 
 
 def engine_binary_name(engine, commit_sha, net_path, private):
-    name = '%s-%s' % (engine, commit_sha.upper()[:8])
+    name = '%s-pro-%s' % (engine, commit_sha.upper()[:8])
     if net_path and not private:
         name += '-%s' % (net_path[-8:])
     return name
